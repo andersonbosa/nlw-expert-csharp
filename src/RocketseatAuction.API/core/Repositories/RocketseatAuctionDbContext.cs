eb757fc28a16e1013bc4a52f634e9ff20af42bf6
andersonbosa <andersonbosa0@gmail.com>
2024-02-17 15:51:47 -0300

    fix getcurrent auction

--- src/RocketseatAuction.API/core/Repositories/RocketseatAuctionDbContext.cs
+++ src/RocketseatAuction.API/core/Repositories/RocketseatAuctionDbContext.cs
@@ -5,10 +5,7 @@ namespace RocketseatAuction.API.Core.Repositories;
 
 public class RocketseatAuctionDbContext : DbContext
 {
-    public RocketseatAuctionDbContext(DbContextOptions options) : base(options)
-    {
-        
-    }
+    public RocketseatAuctionDbContext(DbContextOptions options) : base(options) { }
 
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<User> Users { get; set; }
