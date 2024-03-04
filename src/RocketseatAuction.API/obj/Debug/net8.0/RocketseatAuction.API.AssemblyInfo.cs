adbe2b5e166b9b34984f936debee5d4e4ade3d5d
andersonbosa <andersonbosa0@gmail.com>
2024-03-04 18:08:04 -0300

    chore: add README.md

--- src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
+++ src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d655169229640ddc06f5a905097ab5ffc03ddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7de200e638bd07aeb8dc17e8071d9cc8d68349f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
